c0d721811a9c7cda9ae744925392b7313a5f35b4
User <ssncaoayi@gmail.com>
2024-03-23 19:32:26 +0200

    Added search functionality using the API

--- src/app.js
+++ src/app.js
@@ -1,9 +1,24 @@
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+    let cityElement = document.querySelector("#city")
+    cityElement.innerHTML = response.data.city;
+};
+
+function searchCity(city) {
+    let apiKey = "a0b5205b9cf30bc64d2e1fft3aab382o";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event){
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city")
-    cityElement.innerHTML = searchInput.value;
+    
+    searchCity(searchInput.value);
 }
 
-let serachFormElement = document.querySelector("#search-form");
-serachFormElement.addEventListener("submit", handleSearchSubmit);
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Saldanha");
